6ddb0baf0ea9ad1a899ca2d42aabc529ae5e5739
David Anthoff <anthoff@berkeley.edu>
2018-04-16 17:24:36 -0700

    Progress

--- examples/run_problem3.jl
+++ examples/run_problem3.jl
@@ -1,28 +1,26 @@
-addprocs(4)
+# addprocs(4)
 
-@everywhere using Judyp
-@everywhere using ParallelDataTransfer
-@everywhere using NamedTuples
-@everywhere using NLopt
-@everywhere using Ipopt
+using Judyp
+# @everywhere using NLopt
+# @everywhere using Ipopt
 # using KNITRO
 using Gadfly
-cd("examples")
+# cd("examples")
 include("problem3.jl")
 
 problem = getproblem3()
 
-nlopt_solver = () -> NLoptSolver(algorithm=:LD_SLSQP)
+# nlopt_solver = () -> NLoptSolver(algorithm=:LD_SLSQP)
 # solver=IpoptSolver(hessian_approximation="limited-memory", max_iter=10000, print_level=0, bound_relax_factor=0., linear_solver="ma57")
-ipopt_solver = () -> IpoptSolver(hessian_approximation="limited-memory", max_iter=10000, print_level=0, bound_relax_factor=0.)
+# ipopt_solver = () -> IpoptSolver(hessian_approximation="limited-memory", max_iter=10000, print_level=0, bound_relax_factor=0.)
 # solver=KnitroSolver(hessopt=KTR_HESSOPT_FINITE_DIFF, KTR_PARAM_OUTLEV=0, KTR_PARAM_HONORBNDS=KTR_HONORBNDS_ALWAYS, KTR_PARAM_PAR_CONCURRENT_EVALS=KTR_PAR_CONCURRENT_EVALS_NO, KTR_PARAM_BAR_FEASIBLE=KTR_BAR_FEASIBLE_GET_STAY)
 
 # addprocs()
 
-@time res = psolve(problem, solver_constructors=[nlopt_solver, ipopt_solver], print_level=2)
+@time res = solve(problem, print_level=2)
 
-v = res.valuefun
+# v = res.valuefun
 
-data = [v([i,l]) for i=0.:1.:100., l=0.:1.:100.]
+# data = [v([i,l]) for i=0.:1.:100., l=0.:1.:100.]
 
-plot([x->v([x,10.]), x->v([10.,x])], 0., 100.)
+# plot([x->v([x,10.]), x->v([10.,x])], 0., 100.)
