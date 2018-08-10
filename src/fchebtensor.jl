48f1e36cad35fb557717432fa07034ad647897ae
saqibmtz <37197693+saqibmtz@users.noreply.github.com>
2018-08-10 10:02:37 -0700

    Vector to Matrix

--- src/fchebtensor.jl
+++ src/fchebtensor.jl
@@ -1,13 +1,14 @@
 using ForwardDiff
+using Iterators
 
 mutable struct ValueFunState{NCHOICE}
   n_nodes::Array{Int64,1}
   s_min::Array{Float64,1}
   s_max::Array{Float64,1}
 
-  temp_curr_node::Array{Int64,1}
-  temp_cheb_vals_float64::Array{Array{Float64,1},1}
-  temp_cheb_vals_duals::Array{Array{ForwardDiff.Dual{Void,Float64,NCHOICE},1},1}
+  temp_curr_node::Array{Int64,2}
+  temp_cheb_vals_float64::Array{Float64,2}
+  temp_cheb_vals_duals::Array{ForwardDiff.Dual{Void,Float64,NCHOICE},2}
 end
 
 function cheb_nodes(num_node, a, b)
@@ -15,17 +16,27 @@ function cheb_nodes(num_node, a, b)
 end
 
 function genvaluefunstate(n_nodes, s_min, s_max, nchoice)
+  
+
   vs = ValueFunState{nchoice}(n_nodes, s_min,
     s_max,
-    Array{Int}(length(n_nodes)),
-    Array{Vector{Float64}}(length(n_nodes)),
-    Array{Vector{ForwardDiff.ForwardDiff.Dual{Void,Float64,nchoice}}}(length(n_nodes))
+    Array{Int}(prod(n_nodes),length(n_nodes)),
+    #Pick Max nodes
+    Array{Float64,2}(length(n_nodes),maximum(n_nodes)),
+    Array{ForwardDiff.ForwardDiff.Dual{Void,Float64,nchoice},2}(length(n_nodes),maximum(n_nodes))
     )
 
-  for i=1:length(n_nodes)
-    vs.temp_cheb_vals_float64[i] = Array{Float64}(n_nodes[i])
-    vs.temp_cheb_vals_duals[i] = Array{ForwardDiff.Dual{Void,Float64,nchoice}}(n_nodes[i])
-  end
+    #Tensor Product 
+    arrs = [1:i for i in n_nodes]
+    k=1
+    for v in product(arrs...)
+        i = 1
+        for el in v
+            vs.temp_curr_node[k, i] = el
+            i += 1
+        end
+        k += 1
+    end
 
   return vs
 end
@@ -38,6 +49,9 @@ function getrighttemparray(::Type{ForwardDiff.Dual{Void,Float64,NCHOICE}},state:
   return state.temp_cheb_vals_duals
 end
 
+
+
+
 function valuefun(s_unscaled::Array{T,1}, c::Array{Float64,1}, state::ValueFunState) where {T <: Number}
   n_states = length(state.n_nodes)
   temparray = getrighttemparray(T,state)
@@ -45,50 +59,48 @@ function valuefun(s_unscaled::Array{T,1}, c::Array{Float64,1}, state::ValueFunSt
   # Phase 1
   # Evaluate all the chebyshev basis functions and store the results in temparray
   for l=1:n_states
-    curr_temparray = temparray[l]
 
     # Scale into -1 to 1 range
     z = 2(s_unscaled[l]-state.s_min[l])/(state.s_max[l]-state.s_min[l])-1
 
-    for k=1:state.n_nodes[l]
-      if k==1
-        # Compute degree 0 chebyshev polynomial
-        curr_temparray[k] = 1.
-      elseif k==2
+    
+    if(state.n_nodes[l]>=2)
         # Compute degree 1 chebyshev polynomial
-        curr_temparray[k] = z
-      else
-        # Compute degree n>1 chebyshev polynomial
-        curr_temparray[k] = 2z * curr_temparray[k-1] - curr_temparray[k-2]
-      end
+        temparray[l,2] = z
+        # Compute degree 0 chebyshev polynomial
+        temparray[l,1] = 1.
+
+    elseif(state.n_nodes[l]>=1)
+        # Compute degree 0 chebyshev polynomial
+        temparray[l,1] = 1.
+    end
+
+
+    for k=3:state.n_nodes[l]
+        temparray[l,k] = 2z * temparray[l,k-1] - temparray[l,k-2]
     end
   end
 
-  # Phase 2
-  # Tensor basis related computations
-  ret = zero(T)
 
-  state.temp_curr_node[:] = 1
+  # Phase 2
+  
+
+  ret = zero(T)
+  
   curr_s_pos = 1
+  
+  
   clen = length(c)
   for i=1:clen
     ϕ = zero(T) + c[i]
     for l=1:n_states
-      ϕ *= temparray[l][state.temp_curr_node[l]]
+      ϕ *= temparray[l,state.temp_curr_node[curr_s_pos,l]] 
+      
     end
-
-    # Counting
-    state.temp_curr_node[curr_s_pos] += 1
-    if state.temp_curr_node[curr_s_pos] > state.n_nodes[curr_s_pos] && i!=clen
-      while state.temp_curr_node[curr_s_pos] > state.n_nodes[curr_s_pos]
-        state.temp_curr_node[curr_s_pos] = 1
-        curr_s_pos += 1
-        state.temp_curr_node[curr_s_pos] += 1
-      end
-      curr_s_pos = 1
-    end
+    curr_s_pos += 1
 
     ret = ret + ϕ
   end
   return ret
 end
+
