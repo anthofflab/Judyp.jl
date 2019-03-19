59b658602aa42e3f2351698598c635045fa3e8a5
Alex Schmitt <schmitt@ifo.de>
2019-03-19 02:46:02 +0100

    Updates parameter values in problem4.jl and runs run_all_problems.ipynb

--- examples/problem4.jl
+++ examples/problem4.jl
@@ -8,9 +8,10 @@ function getproblem4()
         δ = 0.1,       # Capital depreciation rate
         A_0 = 1.4,      # Initial level of technology
         k_0 = 0.8,   # Initial level of effective capital K_0/A_0
-        psi = 1,
+        ψ = 1,
         B = 0.621301775,
-        rho = 0.78
+        ρ = 0.78,
+        σ = 0.0067
     )
     
     problem = DynProgProblem()
@@ -20,7 +21,7 @@ function getproblem4()
 
         s_new[1] = (1-p.δ)*s[1] + choices[2]
 
-        s_new[2] = s[2]^p.rho * exp(up[1])
+        s_new[2] = s[2]^p.ρ * exp(up[1])
     end
 
     set_payoff_function!(problem) do s, x, p
@@ -29,7 +30,7 @@ function getproblem4()
         ret = zero(eltype(x))
         consumption = choices[1]
         hours = choices[3]
-        ret += (consumption^(1-p.η))/(1-p.η) - p.B * hours^(1 + p.psi) / (1 + p.psi)
+        ret += (consumption^(1-p.η))/(1-p.η) - p.B * hours^(1 + p.ψ) / (1 + p.ψ)
 
         return ret
     end
@@ -44,8 +45,10 @@ function getproblem4()
         g[1] = Y - choices[1] - choices[2]
     end
 
+    set_exogenous_parameters!(problem, ex_params)
+    
     add_state_variable!(problem, 1., 0.7,1.3, 10)
-    add_state_variable!(problem, 1., 0.887,1.128, 10)
+    add_state_variable!(problem, 1., 0.8853, 1.1295, 10)
 
     add_choice_variable!(problem, 0., 100000000000., 0.1)
     add_choice_variable!(problem, 0., 100000000000., 0.1)
@@ -53,9 +56,11 @@ function getproblem4()
 
     add_constraint!(problem, 0., Inf, false)
 
-    set_uncertain_weights!(problem, [0.2, 0.4, 0.2])
+    ## adds Gauss-Hermite weights and nodes for N = 8
+    set_uncertain_weights!(problem, [1.99604072e-04, 1.70779830e-02, 2.07802326e-01, 6.61147013e-01,6.61147013e-01, 2.07802326e-01, 1.70779830e-02, 1.99604072e-04] ./ sqrt(pi))
 
-    add_uncertain_parameter!(problem, [1.,1.,1.])
+    add_uncertain_parameter!(problem, [-2.93063742, -1.98165676, -1.15719371, -0.38118699,  0.38118699, 1.15719371,  1.98165676,  2.93063742] .* sqrt(2) .* ex_params.σ)
+    
 
 	return problem
 end
