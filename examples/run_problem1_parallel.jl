ed298d1e60f5c40eea8d9709da441d541a1dc558
Alex Schmitt <schmitt@ifo.de>
2020-04-23 16:16:25 +0200

    Modifies examples/run_problem1.jl and examples/run_problem1_parallel.jl: adds a loop to compare running times for different number of state variables

--- examples/run_problem1_parallel.jl
+++ examples/run_problem1_parallel.jl
@@ -8,11 +8,13 @@ using Judyp
 
 include("problem1.jl")
 
-problem = getproblem1()
+for i = 2:3
+problem = getproblem1(i)
 
 nlopt_solver = () -> NLoptSolver(algorithm=:LD_SLSQP)
 ipopt_solver = () -> IpoptSolver(hessian_approximation="limited-memory", max_iter=10000, print_level=0, bound_relax_factor=0.)
 
-@time res = psolve(problem, solver_constructors=[nlopt_solver, ipopt_solver], print_level=2)
+@time res = psolve(problem, solver_constructors=[nlopt_solver, ipopt_solver], print_level=0)
+end
 
-simres = simulate(res, 50)
+# simres = simulate(res, 50, 0)
