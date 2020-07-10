3c28f5a2ff7a041e193d5dced3bc788554a2f47e
David Anthoff <anthoff@berkeley.edu>
2020-07-10 15:55:43 -0700

    Fix typo

--- src/utils.jl
+++ src/utils.jl
@@ -1,6 +1,6 @@
 function our_status(m)
     try
-        return status(mp)
+        return status(m)
     catch err
         @warn "The `status` function has thrown an exception, which it probably shouldn't."
         return :FAILURE
